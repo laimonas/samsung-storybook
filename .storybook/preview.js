884fcfdfdd1ecae2a2ce348746b2bea2368bc93c
Laimonas Mameniskis <v-lmameniskis@nilfisk.com>
2022-03-21 13:29:00 +0200

    Fixes

---
+++ .storybook/preview.js
@@ -0,0 +1,8 @@
+import "../src/styles/fontawesome/fontawesome.css";
+import "../src/basic-components.scss";
+
+export const parameters = {
+    actions: { argTypesRegex: "^on[A-Z].*" },
+    layout: "fullscreen",
+};
+
